bc96a773cb7e1ca8d95959c8fe55d7da44003f7d
RahmaSabry89 <201927204@std.sci.cu.edu.eg>
2023-12-21 14:19:33 +0200

    scope

---
+++ main.js
@@ -0,0 +1,17 @@
+function parent() {
+    let a = 10;
+  
+    function child() {
+      console.log(a);
+      console.log(`From Child ${b}`);
+  
+      function grand() {
+        let b = 100;
+        console.log(`From Grand ${a}`);
+        console.log(`From Grand ${b}`);
+      }
+      grand();
+    }
+    child();
+  }
+  parent();
